e3910853341bd26e0794310b18f7b1b6a471cfab
vigneshdev123coder <vigneshgoud12@gmail.com>
2024-11-26 16:33:20 +0530

    changes

---
+++ task/src/header.js
@@ -0,0 +1,93 @@
+import React from "react";
+
+
+
+function Head(){
+
+    return(
+        <>
+        <div className="name">
+            <h1>VIGNESH GOUD</h1>
+            <h4>Fresher Job Seeker</h4></div>
+     <div className="nav">
+        <a href="#about" target="blank">about</a>
+        <a href="#education">Education</a>
+        <a href="#skills">skills</a>
+        <a href="#projects">projects</a>
+        <a href="#certification">certifications</a>
+        <a href="#contacts">contacts</a>
+        </div>
+                                      
+        <div className="one" id="about">
+            <h2>About Me</h2>
+            <p>To join a reputed organization where I can apply my knowledge and skills to solve real-world challenges, contribute to impactful projects, and grow as a professional while aligning with the company’s vision and goals.</p>
+            </div>        
+            <div className="two" id="education">
+            <h1>    Education</h1>
+            <p> MECHANICAL ENGINEERING,BRILLIANT INSTITUTE OF ENGINEERING AND TECHNOLOGY(CGPA 7.25/10) </p>
+            <p>NISHITHA COLLEGE OF ENGINEERING AND TECHNOLOGY(CGPA 7/10)</p>
+            <p></p>
+            </div>     
+                                      
+<div className="three" id="skills">
+    <h1>skills</h1>
+    <h3>TECHNICAL SKILLS</h3>
+    <p>html,css</p>
+    <p>javascript,sql</p>
+    <h2>soft skills</h2>
+    <p>teamwork</p>
+    <p>responseble person</p>
+    <p>good communication</p>
+    <p>Adaptable</p>
+    <p>Time Management</p>
+    <h2>Version Control</h2>
+    <p>git</p>
+    <p>vs code</p>
+    <p>github</p>
+    </div>    
+
+    <div className="four" id="projects">
+        <h1>projects</h1>
+        <h2>online pooling:</h2>
+        <p>Enables participation from anywhere with an internet connection..</p>
+        <p>Used Natural Language Processing and prompt engineering techniques.</p>
+        <p>Natural Language Processing is utilized to understand user queries.</p>
+        <p>Prompt engineering is employed to train the system.</p>
+        
+        
+        </div>  
+
+        <div  id="certification" className="five" >
+            <h1>certifications</h1>
+            <p>Completed a course in css with a certificate of completion from Great Learning.</p>
+            <p>Completed a course in javascript with a certificate of completion from Great Learning.</p>
+            
+            </div>                   
+    <div id="contacts" className="six">
+        <h1>contacts</h1>
+        <p>Email: <span><a href=" mailto:vigneshgoud12@gmail.com">vigneshgoud12@gmail.com</a></span></p>
+        <p>phone no: <span><a href="tel:91+ 9505455721">9505455721</a></span></p>
+        <p>linkedin :<span><a href="https://www.linkedin.com/in/vignesh-goud-8187212b6/" target="blank">view profile</a></span></p>
+        <p>github: <span><a href="https://github.com/vigneshdev123coder">view repository</a></span></p>
+        <p></p>
+        
+    
+        
+        
+        
+        </div>                            
+
+    <div className="seven">
+
+        <h4>© 2024 vignesh goud. All Rights Reserved.</h4>
+        </div>                                  
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+     </>
+    )
+}
+export default Head;
